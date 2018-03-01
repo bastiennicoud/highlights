3812327b8074324925fb488ce69c2eaae74cf993
Bastien <bastien.nicoud@gmail.com>
2018-03-01 17:34:43 +0100

    Addinf login modal

---
+++ src/components/authentication/LoginForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form action="">
+    <div
+      class="modal-card"
+      style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Login</p>
+      </header>
+      <section class="modal-card-body">
+        <b-field label="Email">
+          <b-input
+            :value="email"
+            type="email"
+            placeholder="Your email"
+            required/>
+        </b-field>
+
+        <b-field label="Password">
+          <b-input
+            :value="password"
+            type="password"
+            password-reveal
+            placeholder="Your password"
+            required/>
+        </b-field>
+
+        <b-checkbox>Remember me</b-checkbox>
+      </section>
+      <footer class="modal-card-foot">
+        <button
+          class="button"
+          type="button"
+          @click="$parent.close()">Close</button>
+        <button class="button is-primary">Login</button>
+      </footer>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  props: {
+    email: {
+      type: String,
+      default: 'tutu'
+    },
+    password: {
+      type: String,
+      default: 'tutu'
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
